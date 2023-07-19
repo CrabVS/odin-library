165bf4a5c2799660e392ecc5e851e269825820f2
CrabVS <139277442+CrabVS@users.noreply.github.com>
2023-07-19 22:05:46 +0200

    Add book constructor

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log('works');
+const Book = function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
