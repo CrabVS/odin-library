9a9dd4f2d8e3d5f15ea4577e18b6b3f2c3a24164
CrabVS <139277442+CrabVS@users.noreply.github.com>
2023-07-19 22:20:04 +0200

    Add book adding functionality

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
+let myLibrary = [];
+
 const Book = function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
 }
+
+const addBook = function addBookToLibrary() {
+    let newBook = new Book(prompt('name'), prompt('author'), prompt('pages'), prompt('read'));
+
+    myLibrary.push(newBook);
+    console.log(myLibrary);
+}
+
+const initializeScript = function initializeScript() {
+    const addBookBtn = document.getElementById('add-btn');
+    addBookBtn.addEventListener('click', () => {
+        addBook();
+    });
+}
+
+initializeScript();
